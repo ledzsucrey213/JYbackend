6abbaec0fa9bea82058482201f73696a5c4bc53e
Yanis Baroudi <yanis.baroudi@etu.umontpellier.fr>
2024-11-15 21:11:49 +0100

    ajout dun compteur de jeux par game_id

--- src/Models/game.js
+++ src/Models/game.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const gameSchema = new Schema({
     name: { type: String, required: true },
     editor: { type: String, required: true },
-    description: { type: String, required: true }
+    description: { type: String, required: true },
+    count: {type: Number, required : true, default : 0}
 });
 
 module.exports = mongoose.model('Game', gameSchema);
