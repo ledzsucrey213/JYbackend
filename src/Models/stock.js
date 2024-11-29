6ec92ba31126c2906d06f19031ba8b1e8175f37c
Yanis Baroudi <yanis.baroudi@etu.umontpellier.fr>
2024-11-29 16:08:17 +0100

    modif stock

--- src/Models/stock.js
+++ src/Models/stock.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const stockSchema = new Schema({
-    games_id: [{ type: String, required: true }],
+    games_id: [{ type: mongoose.Schema.Types.ObjectId, ref: 'GameLabel', required: true }],
     seller_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, unique: true },
-    games_sold: [{ type: String, required : true }] })
+    games_sold: [{ type: mongoose.Schema.Types.ObjectId, ref: 'GameLabel' }]
+});
 
 module.exports = mongoose.model('Stock', stockSchema);
