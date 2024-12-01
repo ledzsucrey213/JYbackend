b51ed49328f8755cf030f9d229c2f8ce11919416
Yanis Baroudi <yanis.baroudi@etu.umontpellier.fr>
2024-12-01 16:29:27 +0100

    modif stock

--- src/Controllers/saleController.js
+++ src/Controllers/saleController.js
@@ -2,7 +2,8 @@ const Sale = require('../Models/sale');
 
 exports.getSales = async (req, res) => {
     try {
-        const sales = await Sale.find().populate('games_id').populate('buyer_id');
+        // Suppression du populate sur buyer_id puisqu'il n'existe plus
+        const sales = await Sale.find().populate('games_id');
         res.status(200).json(sales);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -21,7 +22,8 @@ exports.createSale = async (req, res) => {
 
 exports.getSaleById = async (req, res) => {
     try {
-        const sale = await Sale.findById(req.params.id).populate('games_id').populate('buyer_id');
+        // Suppression du populate sur buyer_id puisqu'il n'existe plus
+        const sale = await Sale.findById(req.params.id).populate('games_id');
         if (!sale) return res.status(404).json({ message: "Sale not found" });
         res.status(200).json(sale);
     } catch (error) {
@@ -32,6 +34,7 @@ exports.getSaleById = async (req, res) => {
 exports.updateSale = async (req, res) => {
     try {
         const updatedSale = await Sale.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!updatedSale) return res.status(404).json({ message: "Sale not found" });
         res.status(200).json(updatedSale);
     } catch (error) {
         res.status(400).json({ message: error.message });
@@ -40,8 +43,9 @@ exports.updateSale = async (req, res) => {
 
 exports.deleteSale = async (req, res) => {
     try {
-        await Sale.findByIdAndDelete(req.params.id);
-        res.status(204).json();
+        const deletedSale = await Sale.findByIdAndDelete(req.params.id);
+        if (!deletedSale) return res.status(404).json({ message: "Sale not found" });
+        res.status(204).json(); // Réponse sans contenu pour une suppression réussie
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
