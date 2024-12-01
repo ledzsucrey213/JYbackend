3c8655ad8b67fff929b1c5273eb87fdc6c65f746
Yanis Baroudi <yanis.baroudi@etu.umontpellier.fr>
2024-12-01 16:20:38 +0100

    modif stock

--- src/Models/sale.js
+++ src/Models/sale.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const saleSchema = new Schema({
     total_price: { type: Number, required: true },
     games_id: [{ type: mongoose.Schema.Types.ObjectId, ref: 'GameLabel', required: true }],
-    buyer_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    buyer_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false },
     sale_date: { type: Date, required: true },
     total_commission: { type: Number, required: true },
     paid_with: { type: String, enum: ['card', 'cash'], required: true }
